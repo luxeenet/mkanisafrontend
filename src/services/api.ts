6e720412afe5c867263ab3a368d41377456781c6
luxeenet <luxeenet@gmail.com>
2026-03-09 22:25:17 +0300

    commitcpmmitcommit

--- src/services/api.ts
+++ src/services/api.ts
@@ -22,4 +22,16 @@ api.interceptors.request.use((config) => {
     return config;
 });
 
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem('accessToken');
+            localStorage.removeItem('tenantId');
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default api;
